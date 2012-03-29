afb398b27ef40d3b6a2e796abe22c0d585fd297f
pku.yantan@gmail.com <pku.yantan@gmail.com@c28351b1-5c43-633c-43ca-583aee4f4860>
2012-03-29 19:48:35 +0000

    version 36 of bumc svn

---  glycresoft/GlycReSoft/FeatureWeight.Designer.cs
+++  glycresoft/GlycReSoft/FeatureWeight.Designer.cs
@@ -68,9 +68,9 @@
             this.label2.Location = new System.Drawing.Point(27, 71);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 16);
+            this.label2.Size = new System.Drawing.Size(89, 16);
             this.label2.TabIndex = 1;
-            this.label2.Text = "NumCharges";
+            this.label2.Text = "avgMonoMW";
             // 
             // label3
             // 
@@ -86,73 +86,67 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(27, 154);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(74, 16);
+            this.label4.Size = new System.Drawing.Size(80, 15);
             this.label4.TabIndex = 3;
-            this.label4.Text = "NumScans";
+            this.label4.Text = "NumCharges";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(27, 191);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(53, 16);
+            this.label5.Size = new System.Drawing.Size(68, 15);
             this.label5.TabIndex = 4;
-            this.label5.Text = "Density";
+            this.label5.Text = "NumScans";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(27, 230);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(85, 16);
+            this.label6.Size = new System.Drawing.Size(47, 15);
             this.label6.TabIndex = 5;
-            this.label6.Text = "TotalVolume";
+            this.label6.Text = "Density";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(27, 266);
             this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(106, 16);
+            this.label7.Size = new System.Drawing.Size(86, 15);
             this.label7.TabIndex = 6;
-            this.label7.Text = "AvgSignalNoise";
+            this.label7.Text = "AvgA:A+2Ratio";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(27, 306);
             this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(99, 16);
+            this.label8.Size = new System.Drawing.Size(76, 15);
             this.label8.TabIndex = 7;
-            this.label8.Text = "AvgA:A+2Ratio";
+            this.label8.Text = "TotalVolume";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.Location = new System.Drawing.Point(27, 343);
             this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(89, 16);
+            this.label9.Size = new System.Drawing.Size(93, 15);
             this.label9.TabIndex = 8;
-            this.label9.Text = "CentroidScan";
+            this.label9.Text = "AvgSignalNoise";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(142, 31);
+            this.label10.Location = new System.Drawing.Point(136, 31);
             this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(51, 15);
@@ -185,7 +179,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(144, 70);
+            this.textBox1.Location = new System.Drawing.Point(138, 70);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(64, 21);
             this.textBox1.TabIndex = 12;
@@ -193,7 +187,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(145, 109);
+            this.textBox2.Location = new System.Drawing.Point(139, 109);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(64, 21);
             this.textBox2.TabIndex = 13;
@@ -201,7 +195,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(145, 148);
+            this.textBox3.Location = new System.Drawing.Point(137, 148);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(64, 21);
             this.textBox3.TabIndex = 14;
@@ -209,7 +203,7 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(145, 185);
+            this.textBox4.Location = new System.Drawing.Point(138, 186);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(64, 21);
             this.textBox4.TabIndex = 15;
@@ -217,7 +211,7 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(145, 224);
+            this.textBox5.Location = new System.Drawing.Point(139, 224);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(64, 21);
             this.textBox5.TabIndex = 16;
@@ -225,7 +219,7 @@
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(145, 260);
+            this.textBox6.Location = new System.Drawing.Point(139, 261);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(64, 21);
             this.textBox6.TabIndex = 17;
@@ -233,7 +227,7 @@
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(145, 300);
+            this.textBox7.Location = new System.Drawing.Point(139, 300);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(64, 21);
             this.textBox7.TabIndex = 18;
@@ -241,7 +235,7 @@
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(145, 340);
+            this.textBox8.Location = new System.Drawing.Point(137, 338);
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(64, 21);
             this.textBox8.TabIndex = 19;
---  glycresoft/GlycReSoft/FeatureWeight.cs
+++  glycresoft/GlycReSoft/FeatureWeight.cs
@@ -77,5 +77,7 @@ namespace GlycreSoft
         {
             this.Close();
         }
+
+
     }
 }
